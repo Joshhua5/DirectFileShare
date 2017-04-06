cf56662c45c7d19c35c92e663f9d2f08e64f6ade
Joshua Waring <joshhua123@gmail.com>
2017-04-07 04:25:27 +0800

    Worked on SharingList, Configuration, New user interface, Logging and Vector.h

--- main.c
+++ main.c
@@ -1,170 +1,67 @@
-#include <stdio.h>
-#include <errno.h>
-#include <dirent.h>
-#include <stdlib.h>
-#include <unistd.h>
+#ifndef MAIN_C
+#define MAIN_C
 
-#include <sys/queue.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h> 
 
 #include "vector.h"
+#include "config.h"
+#include "logger.h"
+#include "vector.h"
+#include "sharinglist.h"
+ 
+int main(int argc, char* argv[]) { 
+	// Initialise the program
+	// Load configuration and allocate 
+	// sharing structre
+	init_sharing_list();
+	Configuration = load_config();
+	start_logging(Configuration.log_file);
+	load_sharing_list(); 
 
-#define MAX_PATH_LENGTH 256
-
-typedef struct Config {
-	char* username;
-	int port;
-	char* ip;
-	char* log_file;
-}Config;
-
-typedef struct SharingList {
-	int list_count;
-} SharingList;
-
-Config create_config(const char* file_name) {
-	Config new_config;
-	new_config.username = "default";
-	new_config.port = 8080;
-	new_config.ip = "192.168.1.1";
-	new_config.log_file = "assignment.log";
-
-	FILE* config_file = fopen(file_name, "w");
-	if (config_file != NULL) {
-
-		fprintf(config_file, "%s%s\n", "USERNAME:", new_config.username);
-		fprintf(config_file, "%s%i\n", "PORT:", new_config.port);
-		fprintf(config_file, "%s%s\n", "IP:", new_config.ip);
-		fprintf(config_file, "%s%s\n", "LOG_FILE:", new_config.log_file);
-
-		fclose(config_file);
-	}
-	return new_config;
-}
-
-Config load_config() {
-	const char* config_name = "settings.conf";
-	// Check for the existance of the file
-	if (access(config_name, F_OK) == 0) {
-		FILE* config_file = fopen(config_name, "r");
-		struct Config loaded_config;
-
-		loaded_config.username = malloc(sizeof(char) * 255);
-		loaded_config.ip = malloc(sizeof(char) * 255);
-		loaded_config.log_file = malloc(sizeof(char) * 255);
-
-		// Detect string length instead of assumming a max of 255
-		// http://stackoverflow.com/questions/1621394/how-to-prevent-scanf-causing-a-buffer-overflow-in-c
-
-		fscanf(config_file, "USERNAME:%255s\n", loaded_config.username);
-		fscanf(config_file, "PORT:%i\n", &(loaded_config.port));
-		fscanf(config_file, "IP:%255s\n", loaded_config.ip);
-		fscanf(config_file, "LOG_FILE:%255s\n", loaded_config.log_file);
-
-		printf("%s\n", loaded_config.username);
-		printf("%i\n", loaded_config.port);
-		printf("%s\n", loaded_config.ip);
-		printf("%s\n", loaded_config.log_file);
-
-		fclose(config_file);
-		return loaded_config;
-	}
-	else {
-		switch (errno) {
-		case ENOENT:
-			// File does not exist
-			return create_config(config_name);
-			break;
-		default:
-			// https://linux.die.net/man/2/access
-			printf("Unable to open log file");
+	// User Interface
+	printf("Legendary File Sharing\n");
+	char user_input[256];
+	char ignore[256];
+	char argument[256];
+	bool running = true;
+	while(running){
+		// Await user input for action
+		printf(">> ");
+		fgets(user_input, 256, stdin);
+		// Add to SharingList
+		if(strstr(user_input, "add")){
+			sscanf(user_input, "%s%s", ignore, argument);
+			if(argument != NULL)
+				add_to_sharing_list(argument);
+		}
+		// Remove from SharingList
+		else if(strstr(user_input, "remove")){
+			sscanf(user_input, "%s%s", ignore, argument);
+			if(argument != NULL)
+				remove_from_sharing_list(argument);
+		// Print out usable functions
+		}else if(strstr(user_input, "help")){ 
+			printf("\tadd [(file)|(folder)] \n");
+			printf("\tremove [(file)|(folder)] \n"); 
+			printf("\tlist\n"); 
+			printf("\texit\n");  
+		// Print out Sharinglist
+		}else if(strstr(user_input, "list")){
+			int i = 0;
+			for(; i < SharingList->count; ++i)
+				printf("%i: %s\n",i, ((FileEntry*)(SharingList->data[i]))->path);
+		}else if(strstr(user_input, "exit")){
+			running = false; 
 		}
 	}
+	// Save SharingList and free allocations
+ 	save_sharing_list();
+	free_config(Configuration);
+	free_sharing_list();
+	stop_logging();
+	return 0; 
 }
 
-void free_config(Config loaded_config) {
-	free(loaded_config.username);
-	free(loaded_config.ip);
-	free(loaded_config.log_file);
-}
-
-void save_sharing_list() {
-
-}
-
-void load_sharing_list() {
-
-}
-
-
-typedef struct DirectoryNode {
-	void* next; // DirectoryNode
-	char path[MAX_PATH_LENGTH];
-} DirectoryNode;
-
-typedef struct FileEntry {
-	char path[MAX_PATH_LENGTH];
-
-} FileEntry;
-
-void scan_directory(char* path) {
-
-	Vector* entry_list = CreateVector();
-	DirectoryNode* dir_list = malloc(sizeof(DirectoryNode));
-	strcpy(dir_list->path, path);
-	dir_list->next = NULL;
-
-	DIR* directory;
-	directory = opendir(dir_list->path);
-	while (dir_list->next != NULL) {
-		// If directory is valid,
-		// add other directories to the list and process entries
-		if (directory != 0) {
-			struct dirent* entry = readdir(directory);
-			if (entry != NULL) {
-				switch (entry->d_type) {
-				case DT_DIR: {
-					DirectoryNode* new_dir = malloc(sizeof(DirectoryNode));
-					new_dir->next = dir_list->next;
-					dir_list->next = new_dir;
-					// Append the folder to the current path
-					strcpy(new_dir->path, dir_list->path);
-					strcat(new_dir->path, "/");
-					strcat(new_dir->path, entry->d_name);
-				}
-							 break;
-				case DT_REG: {
-					FileEntry* new_entry = malloc(sizeof(FileEntry));
-					strcpy(new_entry->path, dir_list->path);
-					strcat(new_entry->path, "/");
-					strcpy(new_entry->path, entry->d_name);
-				}
-							 break;
-				}
-			}
-		}
-		// Move onto the next directory
-		DirectoryNode* old = dir_list;
-		dir_list = dir_list->next;
-		free(old);
-		closedir(directory);
-		directory = opendir(dir_list->path);
-	}
-}
-
-int main(int argc, char* argv[]) {
-	/*
-	Add folder to sharing list: -a [(file)|(folder)]
-	Remove folder from sharing list: -r [(file)|(folder)]
-
-
-	*/
-	  
-	// Config loaded_config = load_config();
-	 
-	int i = 0;
-	for (; i < argc; ++i)
-		printf("%s", argv[i]);
-	 
-	//free_config(loaded_config);
-	return 0;
-}
+#endif
--- vector.h
+++ vector.h
@@ -2,6 +2,7 @@
 #define VECTOR_H
 
 #include <string.h>
+#include "logger.h"
 
 typedef struct Vector {
 	void** data; 
@@ -11,43 +12,88 @@ typedef struct Vector {
 
 Vector* CreateVector(){
 	Vector* vector = malloc(sizeof(Vector));
+	if(vector == NULL){
+		log_entry("Error with vector operation, CreateVector:this");		
+		return NULL;
+	}
 	vector->data = malloc(sizeof(void*) * 10);
+	if(vector->data == NULL){ 
+		log_entry("Error with vector operation, CreateVector:Data");		
+		free(vector);
+		return NULL;
+	}
 	vector->count = 0;
 	vector->capacity = 0; 
 	return vector;
 }
 
+// Removes all entries of NULLptr
+void CompactVector(Vector* vector) { 
+	if(vector == NULL){
+		log_entry("Error with vector operation, CompactVector");
+		return;
+	}
+	int current_shift_count = 0;
+	int i = 0;
+	for (; i < vector->capacity; ++i) {
+		// Shift over NULL pointers, until the end of the vector
+		while (	vector->data[i + current_shift_count] == NULL &&
+				i + current_shift_count < vector->capacity) {
+			current_shift_count++;
+		}
+		vector->data[i] = vector->data[i + current_shift_count];
+	}
+
+	vector->count -= current_shift_count;
+}
+
+void RemoveAtVector(Vector* vector, int index) {
+	vector->data[index] = NULL;
+	CompactVector(vector);
+}
+
 void ExpandVector(Vector* vector, int expanded_count){
+	if(vector == NULL){
+		log_entry("Error with vector operation, ExpandVector");
+		return;
+	}
 	void* new_data = malloc(sizeof(void*) * (expanded_count + vector->capacity));
 	memcpy(new_data, vector->data, sizeof(void*) * vector->capacity);
 	memset(vector->data, 0, sizeof(void*) * vector->capacity);
 	vector->capacity += expanded_count;
 	free(vector->data);
 	vector->data = new_data;
+
+	// Just a precaution
+	CompactVector(vector);
+}
+ 
+void DestroyVector(Vector* vector, void(*destructor)(void*)){	
+	if(vector == NULL){
+		log_entry("Error with vector operation, DestroyVector");
+		return;
+	}
+	if(destructor != NULL){
+		int i = 0;
+		for(; i < vector->count; ++i)
+			(*destructor)(vector->data[i]);
+	}
+	free(vector->data); 
+	free(vector);
 }
 
-void DestroyVector(Vector* list){
-	free(list->data); 
-}
-  
-// Removes all entries of nullptr
-void CompactVector(Vector* list) { 
-	int current_shift_count = 0;
-	int i = 0;
-	for (; i < list->capacity; ++i) {
-		while (	list->data[i + current_shift_count] == NULL &&
-				i + current_shift_count < list->capacity) {
-			current_shift_count++;
-		}
-		list->data[i] = list->data[i + current_shift_count];
+void AppendVector(Vector* vector, void* data){
+	if(vector == NULL){
+		log_entry("Error with vector operation, AppendVector");
+		return;
 	}
 
-	list->count -= current_shift_count;
+	if(vector->count >= vector->capacity - 1){
+		ExpandVector(vector, ((int)(float)vector->capacity * 0.1f) + 5);
+	}
+	vector->data[vector->count++] = data;
 }
+  
 
-void RemoveAtVector(Vector* list, int index) {
-	list->data[index] = NULL;
-	CompactVector(list);
-}
      
 #endif
